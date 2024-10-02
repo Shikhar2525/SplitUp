3342c2a4e71f803f0325b1e0fae2e33a48917d0f
Shikhar Mandloi <shikhar.mandloi@gmail.com>
2024-10-02 23:34:33 +0530

    feat: small change

--- src/components/AddNameModal/AddNameModal.jsx
+++ src/components/AddNameModal/AddNameModal.jsx
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
-import { Modal, Box, Typography, TextField, Button } from "@mui/material";
+import {
+  Modal,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  useMediaQuery,
+} from "@mui/material"; // Import useMediaQuery
 import { useAuth0 } from "@auth0/auth0-react";
 
 // Styling for the modal box
-const modalStyle = {
+const modalStyle = (isSmallScreen) => ({
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: isSmallScreen ? "90%" : 400, // Adjust width based on screen size
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
   borderRadius: 2,
-};
+});
 
 const AddNameModal = ({ open, onClose, onSubmit }) => {
   const [inputValue, setInputValue] = useState("");
   const { logout } = useAuth0();
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm")); // Check if the screen size is small
+
   const handleSubmit = () => {
     onSubmit(inputValue); // Send the input value to the parent
     setInputValue(""); // Reset the input field
@@ -39,11 +48,12 @@ const AddNameModal = ({ open, onClose, onSubmit }) => {
         disableBackdropClick // Prevent backdrop click from closing the modal
         disableEscapeKeyDown // Prevent escape key from closing the modal
       >
-        <Box sx={modalStyle}>
+        <Box sx={modalStyle(isSmallScreen)}>
+          {" "}
+          {/* Pass isSmallScreen to modalStyle */}
           <Typography variant="h6" component="h2" gutterBottom>
-            What's we will call you?
+            What should we call you?
           </Typography>
-
           <TextField
             required
             fullWidth
@@ -51,9 +61,8 @@ const AddNameModal = ({ open, onClose, onSubmit }) => {
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
             margin="normal"
-            helperText="* Its important for us to have you a name"
+            helperText="* It's important for us to have you a name"
           />
-
           <Button
             disabled={!inputValue}
             variant="contained"
@@ -66,7 +75,7 @@ const AddNameModal = ({ open, onClose, onSubmit }) => {
           <Button
             variant="outlined"
             color="error"
-            onClick={handleLogout} // Close button to manually close the modal
+            onClick={handleLogout} // Logout button
             fullWidth
             sx={{ mt: 2 }}
           >
