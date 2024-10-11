d1fdcf9681f0f38fc62e83728719fd5fce35538a
Shikhar Mandloi <shikhar.mandloi@gmail.com>
2024-10-11 13:38:15 +0530

    feat: hiehgt adjustment

--- src/components/GroupBalances/GroupBalances.jsx
+++ src/components/GroupBalances/GroupBalances.jsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { calculateBalances } from "../utils";
 import BalanceCard from "../BalanceCard/BalanceCard";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useCurrentCurrency } from "../contexts/CurrentCurrency";
 
 function GroupBalances({ group }) {
@@ -19,7 +19,7 @@ function GroupBalances({ group }) {
   }, [group, currentCurrency]);
 
   return (
-    <Box sx={{ height: "53vh", overflow: "auto" }}>
+    <Box sx={{ height: "51vh", overflow: "auto" }}>
       <BalanceCard balances={balances} groupId={group?.id} />
     </Box>
   );
